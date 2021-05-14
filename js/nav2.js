03ec2c4850ea66eafc4b09c5a46bf49194477326
BCCanDataAnalytics <83374981+BCCanDataAnalytics@users.noreply.github.com>
2021-05-13 22:23:08 -0700

    Updated with new url structure

--- js/nav2.js
+++ js/nav2.js
@@ -4,12 +4,11 @@ $(document).ready(function() {
     $( this ).addClass("dropdown-menu-" + num);
   });
   
-  $(".dropdown-menu-1").append('<li><a href="https://google.com" target="_blank">New Cancer Diagnosis</a></li>');
-  $(".dropdown-menu-1").append('<li><a href="https://google.com" target="_blank">Crude Rate</a></li>');
-  $(".dropdown-menu-2").append('<li><a href="https://google.com" target="_blank">Mortality Counts</a></li>');
-  $(".dropdown-menu-2").append('<li><a href="https://google.com" target="_blank">Mortality Crude Rate</a></li>');
-  $(".dropdown-menu-3").append('<li><a href="https://google.com" target="_blank">Prevalence Report 1</a></li>');
-  $(".dropdown-menu-3").append('<li><a href="https://google.com" target="_blank">Prevalence Report 2</a></li>');
-  $(".dropdown-menu-4").append('<li><a href="https://google.com" target="_blank">New Cancer Diagnosis</a></li>');
-  $(".dropdown-menu-4").append('<li><a href="https://google.com" target="_blank">Mortality Counts</a></li>');
+  $(".dropdown-menu-1").append('<li><a href="https://bccandataanalytics.shinyapps.io/IncidenceCounts" target="_blank">New Cancer Diagnoses</a></li>');
+  $(".dropdown-menu-1").append('<li><a href="https://bccandataanalytics.shinyapps.io/IncidenceCrudeRates" target="_blank">Crude Rates</a></li>');
+  $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityCounts" target="_blank">Cancer Deaths</a></li>');
+  $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityCrudeRates" target="_blank">Crude Rates</a></li>');
+  $(".dropdown-menu-3").append('<li><a href="https://bccandataanalytics.shinyapps.io/PrevalenceCounts" target="_blank">Number of Prevalent Cases</a></li>');
+  $(".dropdown-menu-4").append('<li><a href="https://bccandataanalytics.shinyapps.io/CreateTableIncidenceCounts" target="_blank">New Cancer Diagnoses</a></li>');
+  $(".dropdown-menu-4").append('<li><a href="https://bccandataanalytics.shinyapps.io/CreateTableMortalityCounts" target="_blank">Cancer Deaths</a></li>');
 });
