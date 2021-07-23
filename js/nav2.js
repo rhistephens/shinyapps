9eb08f1e8c615b210b50a2a162281a28bc8a20e1
BCCanDataAnalytics <83374981+BCCanDataAnalytics@users.noreply.github.com>
2021-07-23 09:43:01 -0700

    added crude rates

--- js/nav2.js
+++ js/nav2.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
   });
   
   $(".dropdown-menu-1").append('<li><a href="https://bccandataanalytics.shinyapps.io/IncidenceCounts" target="_blank">New Cancer Diagnoses</a></li>');
-  /*$(".dropdown-menu-1").append('<li><a href="https://bccandataanalytics.shinyapps.io/IncidenceCrudeRates" target="_blank">Crude Rates</a></li>');*/
+  $(".dropdown-menu-1").append('<li><a href="https://bccandataanalytics.shinyapps.io/IncidenceCrudeRates" target="_blank">Crude Rates</a></li>');
   $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityCounts" target="_blank">Cancer Deaths</a></li>');
-  /*$(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityCrudeRates" target="_blank">Crude Rates</a></li>');*/
+  $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityCrudeRates" target="_blank">Crude Rates</a></li>');
   $(".dropdown-menu-3").append('<li><a href="https://bccandataanalytics.shinyapps.io/PrevalenceCounts" target="_blank">Number of Prevalent Cases</a></li>');
   /*$(".dropdown-menu-4").append('<li><a href="https://bccandataanalytics.shinyapps.io/CreateTableIncidenceCounts" target="_blank">New Cancer Diagnoses</a></li>');*/
   /*$(".dropdown-menu-4").append('<li><a href="https://bccandataanalytics.shinyapps.io/CreateTableMortalityCounts" target="_blank">Cancer Deaths</a></li>');*/
