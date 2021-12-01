3670ed2264f5a13576e6ab521d74a7b9df972967
BCCanDataAnalytics <83374981+BCCanDataAnalytics@users.noreply.github.com>
2021-12-01 12:23:16 -0800

    Update nav2.js
    added projections tab

--- js/nav2.js
+++ js/nav2.js
@@ -12,6 +12,6 @@ $(document).ready(function() {
   $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityCrudeRates" target="_blank">BC: Crude Rates</a></li>');
   $(".dropdown-menu-3").append('<li><a href="https://bccandataanalytics.shinyapps.io/PrevalenceCounts" target="_blank">BC: Number of Prevalent Cases</a></li>');
   $(".dropdown-menu-3").append('<li><a href="https://bccandataanalytics.shinyapps.io/RegionalPrevalenceCounts/" target="_blank">Regional Prevalence</a></li>');
-  /*$(".dropdown-menu-4").append('<li><a href="https://bccandataanalytics.shinyapps.io/CreateTableIncidenceCounts" target="_blank">New Cancer Diagnoses</a></li>');*/
+  $(".dropdown-menu-4").append('<li><a href="https://bccandataanalytics.shinyapps.io/IncidenceProjections/" target="_blank">Estimated New Cancer Diagnoses</a></li>');
   /*$(".dropdown-menu-4").append('<li><a href="https://bccandataanalytics.shinyapps.io/CreateTableMortalityCounts" target="_blank">Cancer Deaths</a></li>');*/
 });
