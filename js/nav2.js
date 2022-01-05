ba47f01bbffe582bc00de7e4c1b08646c72a1598
BCCanDataAnalytics <83374981+BCCanDataAnalytics@users.noreply.github.com>
2022-01-05 12:22:20 -0800

    Update nav2.js

--- js/nav2.js
+++ js/nav2.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
   $(".dropdown-menu-1").append('<li><a href="https://bccandataanalytics.shinyapps.io/RegionalIncidenceCounts/" target="_blank">BC/Regional: New Cancer Diagnoses</a></li>');
   $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityCounts" target="_blank">BC: Cancer Deaths</a></li>');
   $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityCrudeRates" target="_blank">BC: Crude Rates</a></li>');
-  $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/mort_regional_counts_app" target="_blank">BC/Regional: Cancer Deaths</a></li>');
+  $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityRegionalCounts" target="_blank">BC/Regional: Cancer Deaths</a></li>');
   $(".dropdown-menu-3").append('<li><a href="https://bccandataanalytics.shinyapps.io/PrevalenceCounts" target="_blank">BC: Number of Prevalent Cases</a></li>');
   $(".dropdown-menu-3").append('<li><a href="https://bccandataanalytics.shinyapps.io/RegionalPrevalenceCounts/" target="_blank">Regional Prevalence</a></li>');
   $(".dropdown-menu-4").append('<li><a href="https://bccandataanalytics.shinyapps.io/IncidenceProjections/" target="_blank">Estimated New Cancer Diagnoses</a></li>');
