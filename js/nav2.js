0789a236e671c16a205a61dc6b0db7c310e52b82
BCCanDataAnalytics <83374981+BCCanDataAnalytics@users.noreply.github.com>
2022-06-13 14:41:52 -0700

    Update nav2.js

--- js/nav2.js
+++ js/nav2.js
@@ -8,12 +8,10 @@ $(document).ready(function() {
   $(".dropdown-menu-1").append('<li><a href="https://bccandataanalytics.shinyapps.io/IncidenceCrudeRates" target="_blank">BC: Crude Rates</a></li>');
   $(".dropdown-menu-1").append('<li><a href="https://bccandataanalytics.shinyapps.io/IncidenceAgeAdjRates/" target="_blank">BC: Age-standardized Cancer Incidence Rates</a></li>');
   $(".dropdown-menu-1").append('<li><a href="https://bccandataanalytics.shinyapps.io/RegionalIncidenceCounts/" target="_blank">BC/Regional: New Cancer Diagnoses</a></li>');
-  $(".dropdown-menu-1").append('<li><a href="https://bccandataanalytics.shinyapps.io/StandardizedIncidenceRatio/" target="_blank">Standardized Incidence Ratio</a></li>');
   $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityCounts" target="_blank">BC: Cancer Deaths</a></li>');
   $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityCrudeRates" target="_blank">BC: Crude Rates</a></li>');
   $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityAgeStdRates" target="_blank">Age-standardized Cancer Mortality Rates</a></li>');
   $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/MortalityRegionalCounts" target="_blank">BC/Regional: Cancer Deaths</a></li>');
-  $(".dropdown-menu-2").append('<li><a href="https://bccandataanalytics.shinyapps.io/StandardizedMortalityRatio/" target="_blank">Standardized Mortality Ratio</a></li>');
   $(".dropdown-menu-3").append('<li><a href="https://bccandataanalytics.shinyapps.io/PrevalenceCounts" target="_blank">BC: Number of Prevalent Cases</a></li>');
   $(".dropdown-menu-3").append('<li><a href="https://bccandataanalytics.shinyapps.io/RegionalPrevalenceCounts/" target="_blank">Regional Prevalence</a></li>');
   $(".dropdown-menu-4").append('<li><a href="https://bccandataanalytics.shinyapps.io/IncidenceProjections/" target="_blank">Estimated New Cancer Diagnoses</a></li>');
