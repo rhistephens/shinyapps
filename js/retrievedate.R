ffe37294fa714e051ed1ce4bd7ef2e7acd01aa8e
BCCanDataAnalytics <83374981+BCCanDataAnalytics@users.noreply.github.com>
2022-07-06 13:22:15 -0700

    Update retrievedate.R

--- js/retrievedate.R
+++ js/retrievedate.R
@@ -1,4 +1,4 @@
 p("Data Source: BC Cancer Registry")
 p("Date Retrieved:  08 Mar 2022")
-p("Analysis by: Cancer Surviellance and Outcomes, Data and Analytics, BC Cancer")
+p("Analysis by: Cancer Surveillance and Outcomes, Data and Analytics, BC Cancer")
 p("Contact:", tags$a(href="mailto:datareq@bccancer.bc.ca", target="_blank", "datareq@bccancer.bc.ca"))
