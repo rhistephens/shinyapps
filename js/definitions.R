3beb24dc02cf9e60b5c4055905aeb174f4a6e2a1
BCCanDataAnalytics <83374981+BCCanDataAnalytics@users.noreply.github.com>
2023-02-23 15:04:40 -0800

    correct hyperlink to cancer definitions

--- js/definitions.R
+++ js/definitions.R
@@ -1,6 +1,6 @@
 ("Cancer type includes a broader classification based on related organs, such as all organs related to digestive functions are included in 'Digestive Organs'. For a complete classification, please see the",
-                              tags$a(href="http://www.bccancer.bc.ca/statistics-and-reports-site/Documents/cancer%20definitions%20table_20230215.pdf", target="_blank", "glossary".")
+                              tags$a(href="http://www.bccancer.bc.ca/statistics-and-reports-site/Documents/cancer%20definitions%20table_20230215.pdf", target="_blank", "glossary.")
 ("Cancer subtype includes individual organs within the Cancer Type, such as 'Stomach' is a cancer subtype within the cancer type of 'Digestive Organs'. For a complete classification, please see the",
-                              tags$a(href="http://www.bccancer.bc.ca/statistics-and-reports-site/Documents/cancer%20definitions%20table_20230215.pdf", target="_blank", "glossary".")
+                              tags$a(href="http://www.bccancer.bc.ca/statistics-and-reports-site/Documents/cancer%20definitions%20table_20230215.pdf", target="_blank", "glossary.")
 ("Cancer type according to Canadian Cancer Statistics (CSS) grouping, which includes a simple classification based on an organ or group of organs. For a complete classification, please see the",
-                              tags$a(href="http://www.bccancer.bc.ca/statistics-and-reports-site/Documents/cancer%20definitions%20table_20230215.pdf", target="_blank", "glossary".")
+                              tags$a(href="http://www.bccancer.bc.ca/statistics-and-reports-site/Documents/cancer%20definitions%20table_20230215.pdf", target="_blank", "glossary.")
