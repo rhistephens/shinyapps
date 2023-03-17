623cfea3a1ef40aec8eaf918f4952b1fe8671999
BCCanDataAnalytics <83374981+BCCanDataAnalytics@users.noreply.github.com>
2023-03-17 10:06:45 -0700

    Update retrievedate.R

--- js/retrievedate.R
+++ js/retrievedate.R
@@ -1,10 +1,10 @@
 p("Data Source: BC Cancer Registry")
-p("Date Retrieved:  08 Mar 2022")
+p("Date Retrieved:  27 Oct 2022")
 p("Analysis by: Cancer Surveillance and Outcomes, Data and Analytics, BC Cancer")
 p("Contact:", tags$a(href="mailto:datareq@bccancer.bc.ca", target="_blank", "datareq@bccancer.bc.ca"))
 
 # For the apps with data updating every two years, refer to the following code
 p("Data Source: BC Cancer Registry")
-p("Date Retrieved:  08 Mar 2022")
+p("Date Retrieved:  27 Oct 2022")
 p("Analysis by: Cancer Surveillance and Outcomes, Data and Analytics, BC Cancer")
 p("Contact:", tags$a(href="mailto:datareq@bccancer.bc.ca", target="_blank", "datareq@bccancer.bc.ca"))
